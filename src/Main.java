598ecd35db742111c0388392e0c52da1d3fdc683
Perfight <m0nter1327@mail.ru>
2023-01-26 22:11:49 +0300

    l3

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            for (int a = 1; a < 11; a++) {
+                System.out.println(i + "*" + a + " = " + i * a);
+            }
+            System.out.println();
+        }
+    }
+}
